a157eb2dbf5234889ecd781d744ea50203e347cb
Tiffany Obi <tiffanyobi123@gmail.com>
2019-11-14 18:20:34 -0500

    added in mysterword input textfeild and guessword text feild. working on displaying letters one by one as chosen

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -10,11 +10,74 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    @IBOutlet weak var mysteryInputTextField: UITextField!
+    
+    @IBOutlet weak var displayWord: UILabel!
+    
+    
+    @IBOutlet weak var guessedLetterTextField: UITextField!
+    
+    var mysteryWordArray = ""
+    var playerIsPlaying = false
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        mysteryInputTextField.delegate = self
+        guessedLetterTextField.delegate = self
+        
+        displayWord.text = "Let's Begin!"
+        playerIsPlaying = false
   }
 
 
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == mysteryInputTextField {
+            
+        turnMysteryWordToDashes()
+            mysteryInputTextField.text = ""
+            
+            
+        } else if textField == guessedLetterTextField {
+//            guessedLetterTextField.text = ""
+        solveforMysteryWord()
+        
+        }
+        return true
+    }
+    
+    
+    func turnMysteryWordToDashes () {
+        mysteryInputTextField.isSecureTextEntry = true
+        mysteryWordArray.append(mysteryInputTextField.text ?? "")
+              
+        displayWord.text = String(repeating: "_  ", count: mysteryWordArray.count)
+               print(mysteryWordArray)
+               print(displayWord.text ?? "")
+        
+    }
+    
+    func solveforMysteryWord () {
+        
+      var  mysteryWordIndex:Set<Int> = []
+        
+repeat {
+    print(mysteryWordArray)
+       for (index,char) in mysteryWordArray.enumerated() {
+if guessedLetterTextField.text == String(char) {
+            mysteryWordIndex.insert(index)
+//            guessedLetterTextField.text = ""
+            
+            print(mysteryWordIndex)
+        }
+    }
+    guessedLetterTextField.text = ""
+} while playerIsPlaying
+        
+    }
+    
+}
